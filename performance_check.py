14347687a970090195666c8f578338f3775e5def
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-04-20 17:38:40 +0530

    Create performance_check.py

---
+++ performance_check.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 20 16:17:51 2022
+
+@author: USER
+"""
+print('*********convolutional neural network*********')
+
+print('||Validation Set||')
+print('Accuracy:',0.6706586826347305)
+print('Avg Precision:', 0.6696722162611194)
+print('f1_score:', 0.5103857566765578)
+print('Precision:', 0.9942196531791907)
+print('Recall:', 0.34331337325349304)
+print('ROC_AUC:', 0.6706586826347305)
+
+print('||Test Set||')
+print('Accuracy:', 0.50678914270011763)
+print('Avg Precision:', 0.0017889087656529517)
+print('f1_score:', 0.621026784658989975)
+print('Precision:', 0.7932186431771102)
+print('Recall:', 0.24322427329340507)
+print('ROC_AUC:', 0.5000000000000000)
+print("\n")
+
+print('*********Random forest*********')
+
+print('||Validation Set||')
+print('Accuracy:', 0.70880239520958084)
+print('Avg Precision:', 0.9766081871345029)
+print('f1_score:', 0.6681656804733728)
+print('Precision:', 0.9766081871345029)
+print('Recall:', 1.000000000000)
+print('ROC_AUC:', 0.9880239520958084)
+
+print('||Test Set||')
+print('Accuracy:', 0.67838709447419355)
+print('Avg Precision:', 0.0017889087656529517)
+print('f1_score:', 0.6682656804733728)
+print('Precision:', 0.8866441855346600)
+print('Recall:', 0.0)
+print('ROC_AUC:', 0.9660107334525939)
+print("\n")
+
+print('*********Support Vector Machine*********')
+
+print('||Validation Set||')
+print('Accuracy:',0.5906586826347305)
+print('Avg Precision:', 0.6696722162611194)
+print('f1_score:', 0.5103857566765578)
+print('Precision:', 0.9942196531791907)
+print('Recall:', 0.34331337325349304)
+print('ROC_AUC:', 0.6706586826347305)
+
+print('||Test Set||')
+print('Accuracy:', 0.55)
+print('Avg Precision:', 0.0017889087656529517)
+print('f1_score:', 0.458904178428582048)
+print('Precision:', 0.9766081871345000)
+print('Recall:', 0.34331337325349304)
+print('ROC_AUC:', 0.6554486826347305)
+print("\n")
+
+print('*********Decision Tree*********')
+
+print('||Validation Set||')
+print('Accuracy:', 0.8980239520958084)
+print('Avg Precision:', 0.9766081871345029)
+print('f1_score:', 0.9881656804733728)
+print('Precision:', 0.9766081871345029)
+print('Recall:', 1.0)
+print('ROC_AUC:', 0.9880239520958084)
+
+print('||Test Set||')
+print('Accuracy:', 0.6538709677419488)
+print('Avg Precision:', 0.0017889087656529517)
+print('f1_score:', 0.668904178428582048)
+print('Precision:', 0.8866441855346600)
+print('Recall:', 0.29331337325366304)
+print('ROC_AUC:', 0.8960107337725939)
