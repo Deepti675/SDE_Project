bfb8c0048c42a592f9ba14edddc43dae54b211d3
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-04-21 01:07:04 +0530

    Update performance_check.py

--- performance_check.py
+++ performance_check.py
@@ -1,80 +1,95 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 20 16:17:51 2022
 
-@author: USER
-"""
-print('*********convolutional neural network*********')
+import pandas as pd
+import preprocessingfile as preprocess
+import models
 
-print('||Validation Set||')
-print('Accuracy:',0.6706586826347305)
-print('Avg Precision:', 0.6696722162611194)
-print('f1_score:', 0.5103857566765578)
-print('Precision:', 0.9942196531791907)
-print('Recall:', 0.34331337325349304)
-print('ROC_AUC:', 0.6706586826347305)
+data = 'pc2.csv'
+original_data, original_X, original_Y,combined_training_data,x_train1,x_train2,x_train,x_test,x_val,y_train1,y_train2,y_train,y_test,y_val = preprocess.my_sdp_preprocessor(data)
+all_data = [original_data, original_X, original_Y,combined_training_data,x_train1,x_train2,x_train,x_test,x_val,y_train1,y_train2,y_train,y_test,y_val]
 
-print('||Test Set||')
-print('Accuracy:', 0.50678914270011763)
-print('Avg Precision:', 0.0017889087656529517)
-print('f1_score:', 0.621026784658989975)
-print('Precision:', 0.7932186431771102)
-print('Recall:', 0.24322427329340507)
-print('ROC_AUC:', 0.5000000000000000)
-print("\n")
 
-print('*********Random forest*********')
+cnn_clf = models.cnn(*all_data) 
+svm_clf = models.svm(*all_data)
+#rf_clf = models.random_forest(*all_data)
+#nn_clf = models.NN(*all_data)
 
-print('||Validation Set||')
-print('Accuracy:', 0.70880239520958084)
-print('Avg Precision:', 0.9766081871345029)
-print('f1_score:', 0.6681656804733728)
-print('Precision:', 0.9766081871345029)
-print('Recall:', 1.000000000000)
-print('ROC_AUC:', 0.9880239520958084)
 
-print('||Test Set||')
-print('Accuracy:', 0.67838709447419355)
-print('Avg Precision:', 0.0017889087656529517)
-print('f1_score:', 0.6682656804733728)
-print('Precision:', 0.8866441855346600)
-print('Recall:', 0.0)
-print('ROC_AUC:', 0.9660107334525939)
-print("\n")
+from sklearn.metrics import *
 
-print('*********Support Vector Machine*********')
+def print_accuracy(model): #nn,cnn,svm,clf
+    '''if (model == nn_clf):
+        y_pred_on_val = model.predict(x_val)>0.5
+        y_pred_on_test = model.predict(x_test)>0.5'''
+    #elif (model == cnn_clf):
+    if (model == cnn_clf):
+        x_val_matrix = x_val.values
+        x_val1 = x_val_matrix.reshape(x_val_matrix.shape[0], 1, len(x_val.columns), 1)
+        y_pred_on_val = model.predict(x_val1)>0.5
+        x_test_matrix = x_test.values
+        x_test1 = x_test_matrix.reshape(x_test_matrix.shape[0], 1, len(x_test.columns), 1)
+        y_pred_on_test = model.predict(x_test1)>0.5
+    else:
+        y_pred_on_val = model.predict(x_val)
+        y_pred_on_test = model.predict(x_test)
+        
+    print('******', str(model), '******')   
+    print('||Validation Set||')
+    print('Accuracy:',balanced_accuracy_score(y_val,y_pred_on_val))
+    print('Avg Precision:', average_precision_score(y_val,y_pred_on_val))
+    print('f1_score:', f1_score(y_val,y_pred_on_val))
+    print('Precision:', precision_score(y_val,y_pred_on_val))
+    print('Recall:', recall_score(y_val, y_pred_on_val))
+    print('ROC_AUC:',roc_auc_score(y_val,y_pred_on_val))
+    print('||Test Set||')
+    print('Accuracy:',balanced_accuracy_score(y_test,y_pred_on_test))
+    print('Avg Precision:', average_precision_score(y_test,y_pred_on_test))
+    print('f1_score:', f1_score(y_test,y_pred_on_test))
+    print('Precision:', precision_score(y_test,y_pred_on_test))
+    print('Recall:', recall_score(y_test, y_pred_on_test))
+    print('ROC_AUC:',roc_auc_score(y_test,y_pred_on_test))
+    y_pred_on_val_df = pd.DataFrame(y_pred_on_val, columns=['defects1'])
+    y_pred_on_test_df = pd.DataFrame(y_pred_on_test, columns=['defects1'])
+    val_result = pd.concat([y_val['defects'].reset_index(drop=True), y_pred_on_val_df['defects1']],axis=1)
+    val_result = val_result.rename(columns={'defects':'val_actual', 'defects1':'val_predict'})
+    test_result = pd.concat([y_test['defects'].reset_index(drop=True),y_pred_on_test_df['defects1']],axis=1)
+    test_result = test_result.rename(columns={'defects':'test_actual','defects1':'test_predict'})
+    return val_result, test_result
 
-print('||Validation Set||')
-print('Accuracy:',0.5906586826347305)
-print('Avg Precision:', 0.6696722162611194)
-print('f1_score:', 0.5103857566765578)
-print('Precision:', 0.9942196531791907)
-print('Recall:', 0.34331337325349304)
-print('ROC_AUC:', 0.6706586826347305)
 
-print('||Test Set||')
-print('Accuracy:', 0.55)
-print('Avg Precision:', 0.0017889087656529517)
-print('f1_score:', 0.458904178428582048)
-print('Precision:', 0.9766081871345000)
-print('Recall:', 0.34331337325349304)
-print('ROC_AUC:', 0.6554486826347305)
-print("\n")
+svm_val_result, svm_test_result = print_accuracy(svm_clf)
+#rf_val_result, rf_test_result = print_accuracy(rf_clf)
+#nn_val_result, nn_test_result = print_accuracy(nn_clf)
+cnn_val_result, cnn_test_result = print_accuracy(cnn_clf)
 
-print('*********Decision Tree*********')
 
-print('||Validation Set||')
-print('Accuracy:', 0.8980239520958084)
-print('Avg Precision:', 0.9766081871345029)
-print('f1_score:', 0.9881656804733728)
-print('Precision:', 0.9766081871345029)
-print('Recall:', 1.0)
-print('ROC_AUC:', 0.9880239520958084)
+#new_val_set_x = pd.concat([svm_val_result['val_predict'],rf_val_result['val_predict'],nn_val_result['val_predict'],cnn_val_result['val_predict']],axis=1)
+new_val_set_x = pd.concat([svm_val_result['val_predict'],cnn_val_result['val_predict']],axis=1)
+new_val_set_x_matrix = new_val_set_x.values
+new_val_set_y_matrix = svm_val_result['val_actual'].values
 
-print('||Test Set||')
-print('Accuracy:', 0.6538709677419488)
-print('Avg Precision:', 0.0017889087656529517)
-print('f1_score:', 0.668904178428582048)
-print('Precision:', 0.8866441855346600)
-print('Recall:', 0.29331337325366304)
-print('ROC_AUC:', 0.8960107337725939)
+#new_test_set_x = pd.concat([svm_test_result['test_predict'],rf_test_result['test_predict'],nn_test_result['test_predict'],cnn_test_result['test_predict']],axis=1)
+new_test_set_x = pd.concat([svm_test_result['test_predict'],cnn_test_result['test_predict']],axis=1)
+new_test_set_x_matrix = new_test_set_x.values
+new_test_set_y_matrix = svm_test_result['test_actual'].values
+
+
+def send_classifiers_to_LR_file():
+    #return nn_clf, cnn_clf, svm_clf, rf_clf
+    return cnn_clf, svm_clf
+
+from sklearn.linear_model import LogisticRegression
+def send_results_to_logistic_regression():
+    clf = LogisticRegression(random_state=0)
+    clf.fit(new_val_set_x_matrix, new_val_set_y_matrix)
+    #yyy = clf.predict(new_test_set_x_matrix)
+    #accuracy_score(y_test.values,yyy)
+    return clf, new_test_set_x_matrix 
+
+
+
+
+
+
+
+
+
