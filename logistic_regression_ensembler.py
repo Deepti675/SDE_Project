5919c7eefb6a3f7d3f56615027ef6a5068c5b7e7
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-04-22 01:52:05 +0530

    Update logistic_regression_ensembler.py

--- logistic_regression_ensembler.py
+++ logistic_regression_ensembler.py
@@ -1,13 +1,27 @@
-
+#importing pandas
 import pandas as pd
+#importing ensembler
 import performance_check as ensembler
+#importing preprocess
 import preprocessingfile as preprocess
+#importing all packages of sklearn
+import matplotlib.pyplot as plt
 from sklearn.metrics import *
+#loading data
 data = 'pc2.csv'
-original_data, original_X, original_Y,combined_training_data,x_train1,x_train2,x_train,x_test,x_val,y_train1,y_train2,y_train,y_test,y_val = preprocess.my_sdp_preprocessor(data)
-all_data = [original_data, original_X, original_Y,combined_training_data,x_train1,x_train2,x_train,x_test,x_val,y_train1,y_train2,y_train,y_test,y_val]
-
-#nn_clf, cnn_clf, svm_clf, rf_clf = ensembler.send_classifiers_to_LR_file()
+#loading the data
+log=LogisticRegression(penalty='l2',C=.01)
+from sklearn.preprocessing import MinMaxScaler
+min_max=MinMaxScaler()
+original_Y,combined_training_data,x_train1,x_train2,x_train,x_test,x_val,y_train1,y_train2,y_train,y_test,y_val = preprocess.my_sdp_preprocessor(data)
+original_data, original_X = preprocess.my_sdp_preprocessor(data)
+#loading thefara
+from sklearn.linear_model import LogisticRegression
+all_data1 = [original_data, original_X, original_Y,combined_training_data,x_train1,x_train2]
+all_data2 = [x_train,x_test,x_val,y_train1,y_train2,y_train,y_test,y_val]
+X_train[X_train.dtypes[(X_train.dtypes=="float64")|(X_train.dtypes=="int64")]
+                        .index.values].hist(figsize=[11,11])
+nn_clf, cnn_clf, svm_clf, rf_clf = ensembler.send_classifiers_to_LR_file()
 cnn_clf, svm_clf = ensembler.send_classifiers_to_LR_file()
 log_reg_clf, new_test_set_x_matrix = ensembler.send_results_to_logistic_regression()
 
